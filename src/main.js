c141cde83c43c9b6c0c518cf0fbdc6066692d6e9
Maryan Serhii <sergijmara210@gmail.com>
2025-12-21 03:29:22 +0200

    init commit

---
+++ src/main.js
@@ -0,0 +1,7 @@
+import './js/header-hero.js';
+import './js/experience.js';
+import './js/values.js';
+import './js/skillset-portfolio.js';
+import './js/instagram-dribble.js';
+import './js/feedbacs-gallery.js';
+import './js/contacts-footer.js';
