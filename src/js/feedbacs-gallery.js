bb459efd6e886a30cc19229ae94dca9fb8e2f45e
AnastasiiaNotych <notychwork@gmail.com>
2026-01-01 10:48:58 +0100

    finish feedback-gallery section

--- src/js/feedbacs-gallery.js
+++ src/js/feedbacs-gallery.js
@@ -1,9 +1,183 @@
+const galleryData = {
+  italy: [
+    {
+      alt: 'italy alley',
+      desktop:
+        '/img/feedbacs-gallery/gallery/italy/italy-first-desktop.webp    1x,/img/feedbacs-gallery/gallery/italy/italy-first-desktop-2x.webp 2x',
+      tablet:
+        '/img/feedbacs-gallery/gallery/italy/italy-first-tablet.webp    1x,/img/feedbacs-gallery/gallery/italy/italy-first-tablet-2x.webp 2x',
+      mobile:
+        '/img/feedbacs-gallery/gallery/italy/italy-first-mobile.webp    1x,/img/feedbacs-gallery/gallery/italy/italy-first-mobile-2x.webp 2x',
+      img: '/img/feedbacs-gallery/gallery/italy/italy-first-desktop.webp',
+    },
+    {
+      alt: 'venice gondola',
+      desktop:
+        '/img/feedbacs-gallery/gallery/italy/italy-second-desktop.webp    1x,/img/feedbacs-gallery/gallery/italy/italy-second-desktop-2x.webp 2x',
+      tablet:
+        '/img/feedbacs-gallery/gallery/italy/italy-second-tablet.webp    1x,/img/feedbacs-gallery/gallery/italy/italy-second-tablet-2x.webp 2x',
+      mobile:
+        '/img/feedbacs-gallery/gallery/italy/italy-second-mobile.webp    1x,/img/feedbacs-gallery/gallery/italy/italy-second-mobile-2x.webp 2x',
+      img: '/img/feedbacs-gallery/gallery/italy/italy-second-desktop.webp',
+    },
+    {
+      alt: 'italy caffee',
+      desktop:
+        '/img/feedbacs-gallery/gallery/italy/italy-third-desktop.webp    1x,/img/feedbacs-gallery/gallery/italy/italy-third-desktop-2x.webp 2x',
+      tablet:
+        '/img/feedbacs-gallery/gallery/italy/italy-third-tablet.webp    1x,/img/feedbacs-gallery/gallery/italy/italy-third-tablet-2x.webp 2x',
+      mobile:
+        '/img/feedbacs-gallery/gallery/italy/italy-third-mobile.webp    1x,/img/feedbacs-gallery/gallery/italy/italy-third-mobile-2x.webp 2x',
+      img: '/img/feedbacs-gallery/gallery/italy/italy-third-desktop.webp',
+    },
+    {
+      alt: 'italy mountains',
+      desktop:
+        '/img/feedbacs-gallery/gallery/italy/italy-fourth-desktop.webp    1x,/img/feedbacs-gallery/gallery/italy/italy-fourth-desktop-2x.webp 2x',
+      tablet:
+        '/img/feedbacs-gallery/gallery/italy/italy-fourth-tablet.webp    1x,/img/feedbacs-gallery/gallery/italy/italy-fourth-tablet-2x.webp 2x',
+      mobile:
+        '/img/feedbacs-gallery/gallery/italy/italy-fourth-mobile.webp    1x,/img/feedbacs-gallery/gallery/italy/italy-fourth-mobile-2x.webp 2x',
+      img: '/img/feedbacs-gallery/gallery/italy/italy-fourth-desktop.webp',
+    },
+  ],
+  australia: [
+    {
+      alt: 'australia ocean',
+      desktop:
+        '/img/feedbacs-gallery/gallery/australia/australia-first-desktop.webp    1x,/img/feedbacs-gallery/gallery/australia/australia-first-desktop-2x.webp 2x',
+      tablet:
+        '/img/feedbacs-gallery/gallery/australia/australia-first-tablet.webp    1x,/img/feedbacs-gallery/gallery/australia/australia-first-tablet-2x.webp 2x',
+      mobile:
+        '/img/feedbacs-gallery/gallery/australia/australia-first-mobile.webp    1x,/img/feedbacs-gallery/gallery/australia/australia-first-mobile-2x.webp 2x',
+      img: '/img/feedbacs-gallery/gallery/australia/australia-first-desktop.webp',
+    },
+    {
+      alt: 'australia fish',
+      desktop:
+        '/img/feedbacs-gallery/gallery/australia/australia-second-desktop.webp    1x,/img/feedbacs-gallery/gallery/australia/australia-second-desktop-2x.webp 2x',
+      tablet:
+        '/img/feedbacs-gallery/gallery/australia/australia-second-tablet.webp    1x,/img/feedbacs-gallery/gallery/australia/australia-second-tablet-2x.webp 2x',
+      mobile:
+        '/img/feedbacs-gallery/gallery/australia/australia-second-mobile.webp    1x,/img/feedbacs-gallery/gallery/australia/australia-second-mobile-2x.webp 2x',
+      img: '/img/feedbacs-gallery/gallery/australia/australia-second-desktop.webp',
+    },
+    {
+      alt: 'australia beach',
+      desktop:
+        '/img/feedbacs-gallery/gallery/australia/australia-third-desktop.webp    1x,/img/feedbacs-gallery/gallery/australia/australia-third-desktop-2x.webp 2x',
+      tablet:
+        '/img/feedbacs-gallery/gallery/australia/australia-third-tablet.webp    1x,/img/feedbacs-gallery/gallery/australia/australia-third-tablet-2x.webp 2x',
+      mobile:
+        '/img/feedbacs-gallery/gallery/australia/australia-third-mobile.webp    1x,/img/feedbacs-gallery/gallery/australia/australia-third-mobile-2x.webp 2x',
+      img: '/img/feedbacs-gallery/gallery/australia/australia-third-desktop.webp',
+    },
+    {
+      alt: 'australia bridge',
+      desktop:
+        '/img/feedbacs-gallery/gallery/australia/australia-fourth-desktop.webp    1x,/img/feedbacs-gallery/gallery/australia/australia-fourth-desktop-2x.webp 2x',
+      tablet:
+        '/img/feedbacs-gallery/gallery/australia/australia-fourth-tablet.webp    1x,/img/feedbacs-gallery/gallery/australia/australia-fourth-tablet-2x.webp 2x',
+      mobile:
+        '/img/feedbacs-gallery/gallery/australia/australia-fourth-mobile.webp    1x,/img/feedbacs-gallery/gallery/australia/australia-fourth-mobile-2x.webp 2x',
+      img: '/img/feedbacs-gallery/gallery/australia/australia-fourth-desktop.webp',
+    },
+  ],
+  india: [
+    {
+      alt: 'india spices',
+      desktop:
+        '/img/feedbacs-gallery/gallery/india/india-first-desktop.webp    1x,/img/feedbacs-gallery/gallery/india/india-first-desktop-2x.webp 2x',
+      tablet:
+        '/img/feedbacs-gallery/gallery/india/india-first-tablet.webp    1x,/img/feedbacs-gallery/gallery/india/india-first-tablet-2x.webp 2x',
+      mobile:
+        '/img/feedbacs-gallery/gallery/india/india-first-mobile.webp    1x,/img/feedbacs-gallery/gallery/india/india-first-mobile-2x.webp 2x',
+      img: '/img/feedbacs-gallery/gallery/india/india-first-desktop.webp',
+    },
+    {
+      alt: 'india tample',
+      desktop:
+        '/img/feedbacs-gallery/gallery/india/india-second-desktop.webp    1x,/img/feedbacs-gallery/gallery/india/india-second-desktop-2x.webp 2x',
+      tablet:
+        '/img/feedbacs-gallery/gallery/india/india-second-tablet.webp    1x,/img/feedbacs-gallery/gallery/india/india-second-tablet-2x.webp 2x',
+      mobile:
+        '/img/feedbacs-gallery/gallery/india/india-second-mobile.webp    1x,/img/feedbacs-gallery/gallery/india/india-second-mobile-2x.webp 2x',
+      img: '/img/feedbacs-gallery/gallery/india/india-second-desktop.webp',
+    },
+    {
+      alt: 'india valley',
+      desktop:
+        '/img/feedbacs-gallery/gallery/india/india-third-desktop.webp    1x,/img/feedbacs-gallery/gallery/india/india-third-desktop-2x.webp 2x',
+      tablet:
+        '/img/feedbacs-gallery/gallery/india/india-third-tablet.webp    1x,/img/feedbacs-gallery/gallery/india/india-third-tablet-2x.webp 2x',
+      mobile:
+        '/img/feedbacs-gallery/gallery/india/india-third-mobile.webp    1x,/img/feedbacs-gallery/gallery/india/india-third-mobile-2x.webp 2x',
+      img: '/img/feedbacs-gallery/gallery/india/india-third-desktop.webp',
+    },
+    {
+      alt: 'india river and boat',
+      desktop:
+        '/img/feedbacs-gallery/gallery/india/india-fourth-desktop.webp    1x,/img/feedbacs-gallery/gallery/india/india-fourth-desktop-2x.webp 2x',
+      tablet:
+        '/img/feedbacs-gallery/gallery/india/india-fourth-tablet.webp    1x,/img/feedbacs-gallery/gallery/india/india-fourth-tablet-2x.webp 2x',
+      mobile:
+        '/img/feedbacs-gallery/gallery/india/india-fourth-mobile.webp    1x,/img/feedbacs-gallery/gallery/india/india-fourth-mobile-2x.webp 2x',
+      img: '/img/feedbacs-gallery/gallery/india/india-fourth-desktop.webp',
+    },
+  ],
+  brazil: [
+    {
+      alt: 'brazil city',
+      desktop:
+        '/img/feedbacs-gallery/gallery/brazil/brazil-first-desktop.webp    1x,/img/feedbacs-gallery/gallery/brazil/brazil-first-desktop-2x.webp 2x',
+      tablet:
+        '/img/feedbacs-gallery/gallery/brazil/brazil-first-tablet.webp    1x,/img/feedbacs-gallery/gallery/brazil/brazil-first-tablet-2x.webp 2x',
+      mobile:
+        '/img/feedbacs-gallery/gallery/brazil/brazil-first-mobile.webp    1x,/img/feedbacs-gallery/gallery/brazil/brazil-first-mobile-2x.webp 2x',
+      img: '/img/feedbacs-gallery/gallery/brazil/brazil-first-desktop.webp',
+    },
+    {
+      alt: 'brazil flag',
+      desktop:
+        '/img/feedbacs-gallery/gallery/brazil/brazil-second-desktop.webp    1x,/img/feedbacs-gallery/gallery/brazil/brazil-second-desktop-2x.webp 2x',
+      tablet:
+        '/img/feedbacs-gallery/gallery/brazil/brazil-second-tablet.webp    1x,/img/feedbacs-gallery/gallery/brazil/brazil-second-tablet-2x.webp 2x',
+      mobile:
+        '/img/feedbacs-gallery/gallery/brazil/brazil-second-mobile.webp    1x,/img/feedbacs-gallery/gallery/brazil/brazil-second-mobile-2x.webp 2x',
+      img: '/img/feedbacs-gallery/gallery/brazil/brazil-second-desktop.webp',
+    },
+    {
+      alt: 'brazil beach',
+      desktop:
+        '/img/feedbacs-gallery/gallery/brazil/brazil-third-desktop.webp    1x,/img/feedbacs-gallery/gallery/brazil/brazil-third-desktop-2x.webp 2x',
+      tablet:
+        '/img/feedbacs-gallery/gallery/brazil/brazil-third-tablet.webp    1x,/img/feedbacs-gallery/gallery/brazil/brazil-third-tablet-2x.webp 2x',
+      mobile:
+        '/img/feedbacs-gallery/gallery/brazil/brazil-third-mobile.webp    1x,/img/feedbacs-gallery/gallery/brazil/brazil-third-mobile-2x.webp 2x',
+      img: '/img/feedbacs-gallery/gallery/brazil/brazil-third-desktop.webp',
+    },
+    {
+      alt: 'brazil valley',
+      desktop:
+        '/img/feedbacs-gallery/gallery/brazil/brazil-fourth-desktop.webp    1x,/img/feedbacs-gallery/gallery/brazil/brazil-fourth-desktop-2x.webp 2x',
+      tablet:
+        '/img/feedbacs-gallery/gallery/brazil/brazil-fourth-tablet.webp    1x,/img/feedbacs-gallery/gallery/brazil/brazil-fourth-tablet-2x.webp 2x',
+      mobile:
+        '/img/feedbacs-gallery/gallery/brazil/brazil-fourth-mobile.webp    1x,/img/feedbacs-gallery/gallery/brazil/brazil-fourth-mobile-2x.webp 2x',
+      img: '/img/feedbacs-gallery/gallery/brazil/brazil-fourth-desktop.webp',
+    },
+  ],
+};
+
 const galleryButtonList = document.querySelector('.gallery-list-order');
 const galleryButton = galleryButtonList.querySelectorAll(
   '.gallery-order-button'
 );
+const galleryPhotosItem = document.querySelectorAll('.gallery-photos-item');
+const galleryOptionsPhoto = document.getElementById('gallery-options');
 
 galleryButtonList.addEventListener('click', handleGalleryButton);
+galleryOptionsPhoto.addEventListener('click', handleGalleryCountry);
 
 function handleGalleryButton(event) {
   const btn = event.target.closest('.gallery-order-button');
@@ -14,3 +188,29 @@ function handleGalleryButton(event) {
   });
   btn.classList.add('gallery-button-active');
 }
+
+function handleGalleryCountry(event) {
+  const btn = event.target.closest('.gallery-order-button[data-country]');
+  if (!btn) return;
+  const countryKey = btn.dataset.country;
+  showCountry(countryKey);
+}
+
+function showCountry(countryKey) {
+  const item = galleryData[countryKey];
+  if (!item) return;
+
+  galleryPhotosItem.forEach((li, i) => {
+    const sources = li.querySelectorAll('source');
+    const img = li.querySelector('img');
+
+    sources[0].srcset = item[i].desktop;
+    sources[1].srcset = item[i].tablet;
+    sources[2].srcset = item[i].mobile;
+
+    img.src = item[i].img;
+    img.alt = item[i].alt;
+  });
+}
+
+showCountry('italy');
